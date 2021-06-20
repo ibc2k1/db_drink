21dc9cfcaef2d8b0466572c60510d70eb308256c
ibc2k1 <ireneb2k1@gmail.com>
2021-06-21 00:33:24 +0800

    new

---
+++ src/page/ManagePage/manage.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function SignIn(){
+  return(
+    <>
+    <h1>hello</h1>
+    <a href = "http://54.88.4.245/webmaster/login.php">Login</a>
+    </>
+  )
+}
